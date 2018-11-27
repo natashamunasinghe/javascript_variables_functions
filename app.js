7823dec794dd84fd1bd3598119ebf539d250bb1c
Natasha <natashakhema@gmail.com>
2018-11-28 09:44:44 +1100

    JS introduction

---
+++ app.js
@@ -0,0 +1,18 @@
+
+//variable
+let a = "mouse";
+
+//named function
+function b() {
+    console.log("Called b!");
+}
+
+//invoke a function
+b();
+console.log(a);
+
+if (a === undefined) {
+    console.log("a is undefined");
+} else {
+    console.log("a is defined");
+}
